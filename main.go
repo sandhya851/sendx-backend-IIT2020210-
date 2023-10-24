a2aa3ffbc49f3d8ebb216e235752c44ae8d5f702
sandhya851 <75798861+sandhya851@users.noreply.github.com>
2023-10-24 16:48:38 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"strings"
+	"golang.org/x/net/html"
 
 	"github.com/gorilla/mux"
 	"github.com/PuerkitoBio/goquery"
@@ -51,4 +53,31 @@ func crawlHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Real-time Crawled Page Data: %s", pageData)
+
+
+func main() {
+	htmlContent := `<html><body><h1>Hello, World!</h1></body></html>`
+
+	// Parse the HTML content
+	doc, err := html.Parse(strings.NewReader(htmlContent))
+	if err != nil {
+		fmt.Println("Error parsing HTML:", err)
+		return
+	}
+	// Traverse the HTML parse tree
+	traverseHTML(doc)
+}
+	func traverseHTML(node *html.Node) {
+    if node.Type == html.ElementNode {
+        // Do something with the element node, e.g., extract attributes or text content
+        fmt.Println("Element:", node.Data)
+    }
+
+    // Recursive traversal for child nodes
+    for c := node.FirstChild; c != nil; c = c.NextSibling {
+        traverseHTML(c)
+    }
+}
+
+
 }
