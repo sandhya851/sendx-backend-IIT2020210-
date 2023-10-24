12dfaf302cde0e88ad2c442ed82a334801be004b
sandhya851 <75798861+sandhya851@users.noreply.github.com>
2023-10-24 17:24:40 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,83 +1,27 @@
-// main.go
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"time"
-	"strings"
-	"golang.org/x/net/html"
-
-	"github.com/gorilla/mux"
-	"github.com/PuerkitoBio/goquery"
+    "html/template"
+    "log"
+    "net/http"
 )
 
-type CrawlCache struct {
-	LastCrawled time.Time
-	PageData   string
-}
-
-var cache map[string]CrawlCache
-
 func main() {
-	cache = make(map[string]CrawlCache)
-
-	r := mux.NewRouter()
-	r.HandleFunc("/crawl", crawlHandler).Methods("GET")
-	http.Handle("/", r)
-
-	http.ListenAndServe(":8080", nil)
-}
-
-func crawlHandler(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("url")
-	isPayingCustomer := r.URL.Query().Get("paying_customer")
-
-	if cacheData, ok := cache[url]; ok && time.Since(cacheData.LastCrawled).Minutes() < 60 {
-		// Page exists in cache, return cached page
-		fmt.Fprintf(w, "Cached Page Data: %s", cacheData.PageData)
-		return
-	}
-
-	// Page not in cache, crawl in real-time
-	doc, err := goquery.NewDocument(url)
-	if err != nil {
-		fmt.Fprintf(w, "Error fetching URL: %s", err.Error())
-		return
-	}
-
-	pageData := doc.Text()
-	cache[url] = CrawlCache{
-		LastCrawled: time.Now(),
-		PageData:   pageData,
-	}
-
-	fmt.Fprintf(w, "Real-time Crawled Page Data: %s", pageData)
-
-
-func main() {
-	htmlContent := `<html><body><h1>Hello, World!</h1></body></html>`
-
-	// Parse the HTML content
-	doc, err := html.Parse(strings.NewReader(htmlContent))
-	if err != nil {
-		fmt.Println("Error parsing HTML:", err)
-		return
-	}
-	// Traverse the HTML parse tree
-	traverseHTML(doc)
-}
-	func traverseHTML(node *html.Node) {
-    if node.Type == html.ElementNode {
-        // Do something with the element node, e.g., extract attributes or text content
-        fmt.Println("Element:", node.Data)
+    // Parse the HTML template
+    tmpl, err := template.ParseFiles("templates/index.html")
+    if err != nil {
+        log.Fatal("Error parsing template:", err)
     }
 
-    // Recursive traversal for child nodes
-    for c := node.FirstChild; c != nil; c = c.NextSibling {
-        traverseHTML(c)
-    }
-}
+    // Handle static files (CSS, JS, images, etc.)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
+    // Handle the root URL with the parsed template
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // Execute the template, passing nil as data
+        tmpl.Execute(w, nil)
+    })
 
+    // Start the HTTP server
+    http.ListenAndServe(":8080", nil)
 }
