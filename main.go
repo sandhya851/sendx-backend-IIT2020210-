b7c791a05bacf74542f49695e05c568a3dea1882
sandhya851 <75798861+sandhya851@users.noreply.github.com>
2023-10-24 17:50:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
     "html/template"
     "log"
     "net/http"
+    "github.com/yourusername/web-crawler/crawler"
+    "github.com/yourusername/web-crawler/server"
+    "github.com/yourusername/web-crawler/api"
 )
 
 func main() {
@@ -13,6 +16,11 @@ func main() {
         log.Fatal("Error parsing template:", err)
     }
 
+    // Initialize Crawler, Server, and API instances
+    c := crawler.NewCrawler()
+    s := server.NewServer(c)
+    a := api.NewAPI()
+
     // Handle static files (CSS, JS, images, etc.)
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
@@ -21,6 +29,12 @@ func main() {
         // Execute the template, passing nil as data
         tmpl.Execute(w, nil)
     })
+
+    // Set up API routes
+    http.HandleFunc("/crawl", a.CrawlHandler)
+    // Add other API endpoints if needed
+    http.HandleFunc("/set-worker-count", s.SetWorkerCountHandler)
+    http.HandleFunc("/set-crawl-speed", s.SetCrawlSpeedHandler)
 
     // Start the HTTP server
     http.ListenAndServe(":8080", nil)
